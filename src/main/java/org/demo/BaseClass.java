5fff9906d0ef2d68838bbc5e818c35d79dcf67fb
vasanthsudalai <msvasanth.mech@gmail.com>
2023-02-08 12:19:59 +0530

    msvasanth's code

--- src/main/java/org/demo/BaseClass.java
+++ src/main/java/org/demo/BaseClass.java
@@ -1,5 +1,10 @@
 package org.demo;
 
 public class BaseClass {
+public static void main(String[] args) {
+	System.out.println("Done Testing 11");
+	System.out.println("Done Testing 12");
+	System.out.println("Done Testing 13");
 
+}
 }
--- src/test/java/org/example/LoginPage.java
+++ src/test/java/org/example/LoginPage.java
@@ -1,5 +1,9 @@
 package org.example;
 
 public class LoginPage {
-
+public static void main(String[] args) {
+	System.out.println("Test 4");
+	System.out.println("Test 5");
+	System.out.println("Test 6");
+}
 }
