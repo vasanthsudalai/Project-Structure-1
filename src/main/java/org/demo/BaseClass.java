d1fbe4695d095e17a43c5dd1f02e1b9906863fc9
vasanthsudalai <msvasanth.mech@gmail.com>
2023-02-08 12:04:29 +0530

    Project Structure 1

--- src/main/java/org/demo/BaseClass.java
+++ src/main/java/org/demo/BaseClass.java
@@ -1,5 +1,10 @@
 package org.demo;
 
 public class BaseClass {
+public static void main(String[] args) {
+	System.out.println("Done testing 1");
+	System.out.println("Done testing 2");
+	System.out.println("Done testing 3");
 
+}
 }
